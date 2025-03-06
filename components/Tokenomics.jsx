9b9cf126776585c17f911ec175eaf3b2189fa4ae
knulloff <knulloff@gmail.com>
2025-03-06 20:43:50 +0100

    Update Tokenomics.jsx

--- components/Tokenomics.jsx
+++ components/Tokenomics.jsx
@@ -9,37 +9,37 @@ const Tokenomics = ({ styles }) => {
         <div className={styles.tokenomicsDetailsHolder}>
           <div className={styles.tokenomicsColumn}>
             <h4 className={styles.tokenDetailHead}>Token Name</h4>
-            <p className={styles.tokenDetailText}>KREDICT</p>
+            <p className={styles.tokenDetailText}>STARFROG</p>
             <h4 className={styles.tokenDetailHead}>Total Supply</h4>
             <p className={styles.tokenDetailText}>1,000,000,000,000,000</p>
             <h4 className={styles.tokenDetailHead}>Pre Sale Start Date</h4>
-            <p className={styles.tokenDetailText}>1st Oct 2022</p>
+            <p className={styles.tokenDetailText}>1st Mar 2025</p>
             <h4 className={styles.tokenDetailHead}>Currencies Accepted</h4>
-            <p className={styles.tokenDetailText}>BNB</p>
+            <p className={styles.tokenDetailText}>SOL</p>
           </div>
           <div className={styles.tokenomicsColumn}>
             <h4 className={styles.tokenDetailHead}>
-              ICO Pre- Sale KREDICT Supply Launch
+              ICO Pre- Sale STARFROG Supply Launch
             </h4>
-            <p className={styles.tokenDetailText}>200,000,000,000,000</p>
+            <p className={styles.tokenDetailText}>200,000,000</p>
             <h4 className={styles.tokenDetailHead}>
-              ICO Supply OF KREDICT launch
+              ICO Supply OF STARFROG launch
             </h4>
-            <p className={styles.tokenDetailText}>200,000,000,000,000</p>
+            <p className={styles.tokenDetailText}>200,000,000</p>
             <h4 className={styles.tokenDetailHead}>Soft cap</h4>
-            <p className={styles.tokenDetailText}>4000 BNB</p>
+            <p className={styles.tokenDetailText}>4000 SOL</p>
             <h4 className={styles.tokenDetailHead}>ICO Start Date</h4>
-            <p className={styles.tokenDetailText}>1st of Jan 2023</p>
+            <p className={styles.tokenDetailText}>1st of Jun 2025</p>
           </div>
           <div className={styles.tokenomicsColumn}>
             <h4 className={styles.tokenDetailHead}>Pre Sale - 1st Phase</h4>
-            <p className={styles.tokenDetailText}>1 BNB = 2,600,000,000 KDT</p>
+            <p className={styles.tokenDetailText}>1 SOL = 2,600,000 STF</p>
             <h4 className={styles.tokenDetailHead}>Pre Sale - 2nd Phase</h4>
-            <p className={styles.tokenDetailText}>1 BNB = 1,300,000,000 KDT</p>
+            <p className={styles.tokenDetailText}>1 SOL = 1,300,000 STF</p>
             <h4 className={styles.tokenDetailHead}>Pre Sale - 3rd Phase</h4>
-            <p className={styles.tokenDetailText}>1 BNB = 650,000,000 KDT</p>
+            <p className={styles.tokenDetailText}>1 SOL = 650,000 STF</p>
             <h4 className={styles.tokenDetailHead}>Pre Sale- 4th phase</h4>
-            <p className={styles.tokenDetailText}> 1 BNB = 325,000,000 KDT</p>
+            <p className={styles.tokenDetailText}> 1 SOL = 325,000 STF</p>
           </div>
         </div>
         <p>
